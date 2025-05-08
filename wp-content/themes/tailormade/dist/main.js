6fb11a932a3245e7595f63ef5ed4ddb9d31d09a3
erinwight <eeh9313@yahoo.com>
2025-05-08 16:05:51 +0000

    form update ccpa

--- wp-content/themes/tailormade/dist/main.js
+++ wp-content/themes/tailormade/dist/main.js
@@ -44078,7 +44078,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46301" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43491" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
