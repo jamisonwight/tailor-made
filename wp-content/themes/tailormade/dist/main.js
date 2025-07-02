21bf9ea6861667be4ec66e1b22eec1ecf7f28468
Jamison Wight <jamisonw.wight@gmail.com>
2025-07-02 18:35:17 +0000

    Fix footer link dividers

--- wp-content/themes/tailormade/dist/main.js
+++ wp-content/themes/tailormade/dist/main.js
@@ -44078,7 +44078,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40751" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41451" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
